bdc682eaef4e6e99d17d37f604487d7b6af3c47c
= <=>
2020-11-03 03:54:29 -0400

    Implementando findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,16 +11,11 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1,"Livros");
-		
-		Seller seller = new Seller(1,"Lino", "linomota0@gmail.com", new Date(), 3000.0, obj);
-
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(2);
 		
-		System.out.println(obj.toString());
-		
-		System.out.println(seller.toString());
+		System.out.println(seller);
 		
 	}
 
